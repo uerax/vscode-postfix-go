c79418f7f6a4f6e1cacbcc1fa29005dbd603a53a
Sota Yokoe <sota@kreuz45.com>
2018-07-30 22:06:03 +0900

    feat: Add AppendTemplate.

---
+++ src/templates/appendTemplate.ts
@@ -0,0 +1,15 @@
+import * as vsc from 'vscode'
+import { CompletionItemBuilder } from '../completionItemBuilder'
+import { BaseExpressionTemplate } from './baseTemplates'
+
+export class AppendTemplate extends BaseExpressionTemplate {
+  buildCompletionItem (code: string, position: vsc.Position) {
+    return CompletionItemBuilder
+      .create('append', code)
+      .description(`expr = append(expr, ...)`)
+      .replace(`{{expr}} = append({{expr}}, \${0:element})`, position, true)
+      .build()
+  }
+}
+
+export const build = () => new AppendTemplate()
