5d0225f9b062a47de173f35926bde40b6ecac455
Sota Yokoe <sota@kreuz45.com>
2018-08-26 00:51:05 +0900

    feat: Added `interface` template. Closes #4.

---
+++ src/templates/typeTemplates.ts
@@ -0,0 +1,23 @@
+import * as vsc from 'vscode'
+import { CompletionItemBuilder } from '../completionItemBuilder'
+import { BaseExpressionTemplate } from './baseTemplates'
+import { getIndentCharacters } from '../utils'
+
+export class TypeTemplate extends BaseExpressionTemplate {
+  constructor (private keyword: string) {
+    super()
+  }
+
+  buildCompletionItem (code: string, position: vsc.Position) {
+    return CompletionItemBuilder
+      .create(this.keyword, code)
+      .description(`type expr ${this.keyword}`)
+      .replace(`type {{expr}} ${this.keyword} {\n${getIndentCharacters()}\${0}\n}`, position, true)
+      .build()
+  }
+}
+
+export const build = () => [
+  new TypeTemplate('struct'),
+  new TypeTemplate('interface'),
+]
