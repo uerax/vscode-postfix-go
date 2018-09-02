8b2dfd20f968812999053bfb9faca910acbe9430
Sota Yokoe <sota@kreuz45.com>
2018-09-02 21:43:59 +0900

    fix: `must` template was unintentionally added in commit 6522f60. This is removed at the moment and will be discussed later.

--- src/templates/errorTemplate.ts
+++ src/templates/errorTemplate.ts
@@ -13,17 +13,6 @@ export class ErrorTemplate extends BaseExpressionTemplate {
   }
 }
 
-export class MustTemplate extends BaseExpressionTemplate {
-  buildCompletionItem (code: string, position: vsc.Position) {
-    return CompletionItemBuilder
-      .create('must', code)
-      .description(`Must checks for nil and returns if expression != nil.`)
-      .replace(`if \${1:{{expr}}} != nil {\n${getIndentCharacters()}return \${2:nil}, \${1:{{expr}}}\${0}\n}`, position, true)
-      .build()
-  }
-}
-
 export const build = () => [
-  new ErrorTemplate(),
-  new MustTemplate()
+  new ErrorTemplate()
 ]
