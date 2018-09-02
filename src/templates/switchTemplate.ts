84a01e435af9bbea3c9f1d5a25c38158a7b1e6ae
Sota Yokoe <sota@kreuz45.com>
2018-09-02 21:38:08 +0900

    refactor: SwitchTemplate should not extends BaseForTemplate.

--- src/templates/switchTemplate.ts
+++ src/templates/switchTemplate.ts
@@ -3,21 +3,17 @@ import { CompletionItemBuilder } from '../completionItemBuilder'
 import { BaseExpressionTemplate, BaseTemplate } from './baseTemplates'
 import { getIndentCharacters } from '../utils'
 
-abstract class BaseForTemplate extends BaseTemplate {
-  abstract buildCompletionItem (code: string, position: vsc.Position, suffix: string)
-
-  canUse (code: string): boolean {
-    return true
-  }
-}
-
-export class SwitchTemplate extends BaseForTemplate {
+export class SwitchTemplate extends BaseTemplate {
   buildCompletionItem (code: string, position: vsc.Position) {
     return CompletionItemBuilder
       .create('switch', code)
       .description('switch statemeent (Go)')
       .replace(`switch \${1:{{expr}}} {\ncase \${2:condition}: \n\${0}\n}`, position, true)
       .build()
+  }
+
+  canUse (code: string): boolean {
+    return true
   }
 }
 
